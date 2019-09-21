987ee60530559df0a2929072d7985c11b778f8c2
Okiki Ojo <femikiki@gmail.com>
2019-09-21 13:49:56 -0400

    Push

--- src/js/components/partial.js
+++ src/js/components/partial.js
@@ -6,31 +6,33 @@ let { state, replaceState, pushState } = window.history;
 let { href, origin } = window.location;
 let { title } = document;
 
-// A transition manager [based on smoothState.js]
-let _partial = _event.extend({
-    _class: "Partial-Transition-Manager",
-    defaults: {
+// A transition manager [based on smoothState.js, swup.js & barba.js]
+export default class _partial extends _event {
+    // _class: "Partial-Transition-Manager",
+    static defaults = {
         headers: {
             "x-partial": "html"
         },
         containers: ["[data-partial]"],
-        anchors: `a[href^="${origin}"]:not([data-partial-default]), 
-              a[href^="/"]:not([data-partial-default]), 
+        anchors: `a[href^="${origin}"]:not([data-partial-default]),
+              a[href^="/"]:not([data-partial-default]),
               a[href^="#"]:not([data-partial-default])`,
         enable: true,
         cache: true,
         events: {}
-    },
-    init(opts) {
+    };
+
+    constructor(opts) {
         this.opts = assign({}, this.defaults, opts);
         this.scrollEle = null;
         this.transition = {};
 
         if (this.opts.enable) this.enable();
-    },
+    }
+
     enable() {
         if (_is.undef(pushState)) return this;
     }
-});
-    
-export default _partial;
+}
+
+export let _partial = _parti;
