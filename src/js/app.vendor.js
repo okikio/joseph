c51d36d26e264951f9ad144ea93b01cb6992c9b1
Okiki <femikiki@gmail.com>
2019-09-28 06:38:37 +0000

    WIP

--- src/js/app.vendor.js
+++ src/js/app.vendor.js
@@ -41,7 +41,9 @@ try {
     }
 
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.min.js')
+        navigator.serviceWorker.register('./sw.min.js', {
+                scope: './'
+            })
             .then(function (registration) {
                 console.log('Registration successful, scope is:', registration.scope);
             })
@@ -64,21 +66,21 @@ try {
             deferredPrompt = e;
             // Update UI to notify the user they can add to home screen
             addBtn.style.display = 'block';
+        });
 
-            addBtn.addEventListener('click', (e) => {
-                // hide our user interface that shows our A2HS button
-                addBtn.style.display = 'none';
-                // Show the prompt
-                deferredPrompt.prompt();
-                // Wait for the user to respond to the prompt
-                deferredPrompt.userChoice.then((choiceResult) => {
-                    if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the A2HS prompt');
-                    } else {
-                    console.log('User dismissed the A2HS prompt');
-                    }
-                    deferredPrompt = null;
-                });
+        addBtn.addEventListener('click', (e) => {
+            // hide our user interface that shows our A2HS button
+            addBtn.style.display = 'none';
+            // Show the prompt
+            deferredPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+                } else {
+                console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
             });
         });
     }
