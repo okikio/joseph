effc3a556841c85f2a6292cfdfa5de2d97a1428e
Okiki <femikiki@gmail.com>
2019-11-19 21:18:46 +0000

    Done

--- config-dev.js
+++ config-dev.js
@@ -128,6 +128,52 @@ module.exports = {
                                     }],
                                     "type": "main"
                                 }
+                            }, {
+                                "section": {
+                                    "values": [{
+                                        "content": " ",
+                                        "class": "layout-padding-top layout-margin-left-large layout-inline-block"
+                                    }, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer\n                                        took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.\n                                        It was popularised in the 1960s with the release of Letraset sheets\n                                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum, ", {
+                                        "a": {
+                                            "href": "/index.html",
+                                            "content": "home"
+                                        }
+                                    }],
+                                    "type": "main"
+                                }
+                            }, {
+                                "section": {
+                                    "title": "Lorem Iptsium",
+                                    "type": "header"
+                                }
+                            }, {
+                                "section": {
+                                    "values": ["Github: ", {
+                                        "a": {
+                                            "href": "/index.html",
+                                            "content": "home"
+                                        }
+                                    }, "<br>", "Youtube: ", {
+                                        "a": {
+                                            "href": "/index.html",
+                                            "content": "home"
+                                        }
+                                    }],
+                                    "type": "main"
+                                }
+                            }, {
+                                "section": {
+                                    "values": [{
+                                        "content": " ",
+                                        "class": "layout-padding-top layout-margin-left-large layout-inline-block"
+                                    }, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer\n                                        took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.\n                                        It was popularised in the 1960s with the release of Letraset sheets\n                                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum, ", {
+                                        "a": {
+                                            "href": "/index.html",
+                                            "content": "home"
+                                        }
+                                    }],
+                                    "type": "main"
+                                }
                             }]
                         }
                     }
