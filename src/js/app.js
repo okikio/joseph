267cb4aaf7b9c07a8953f87eba8246fb00eedb29
Okiki Ojo <femikiki@gmail.com>
2020-01-30 15:48:03 -0500

    Oh well...

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,46 @@ let _backUp = '.back-to-top';
 let _actioncenter = ".layer-action-center";
 let _scrolldown = '.layer-hero-scroll-down';
 
+let ready, resize;
+let _focusPt = 70;
+let _images = [];
+let onload = $load_img => function () {
+    addClass($load_img, "core-img-show");
+};
+
+resize = () => {
+    toggleClass(_scrolldown, "action-btn-expand", width(window) <= 650);
+};
+
+ready = () => {
+    let _scrollTop = scrollTop(window);
+    hasClass(_navbar, "banner-mode") && addClass(_navbar, "navbar-focus") || toggleClass(_navbar, "navbar-focus", _scrollTop >= 5);
+    hasClass(_navbar, "navbar-show") && removeClass(_navbar, "navbar-show");
+    resize();
+
+    toggleClass(_actioncenter, "layer-action-center-show", _scrollTop > _focusPt * 4);
+    toggleClass(_actioncenter, "layer-action-center-hide", _scrollTop <= _focusPt * 4);
+
+    if (width(window) > 500) {
+        _images.forEach(data => {
+            if (hasClass(data.target, "effect-parallax")) {
+                let { clientRect, load_img, overlay, isHero } = data;
+                let { top, height } = clientRect;
+                let dist = _scrollTop - top + 60;
+
+                if (dist >= -_focusPt && dist <= height - _focusPt) {
+                    let value = Math.round(_map(_constrain(dist, 0, height), 0, height, 0, 1) * 100) / 100;
+
+                    isHero && style(overlay, { opacity: _map(value, 0, 0.75, 0.55, 0.15) });
+                    style(load_img, {
+                        transform: `translateY(${_map(_constrain(value - _map(60, 0, height, 0, 1), 0, 1), 0, 1, 0, height / 2)}px)`,
+                    });
+                }
+            }
+        });
+    }
+};
+
 let _load = () => {
     //  touchstart
     on(_menu, "click", () => {
@@ -24,33 +64,29 @@ let _load = () => {
         scrollTo("0px", "1400ms");
     });
 
-    let _focusPt = height(_navbar) + 10;
+    _focusPt = height(_navbar) + 10;
 
     // touchstart
     on(_scrolldown, "click", () => {
         scrollTo(height(_hero) - _focusPt, "800ms");
     });
 
-    let _images = [];
-    let onload = $load_img => function () {
-        addClass($load_img, "core-img-show");
-    };
-
+    _images = [];
     each(_layer, $layer => {
         let layer_image = find($layer, ".layer-image");
         let isHero = hasClass($layer, "layer-hero-id");
 
-        each(layer_image, $img => {
+        each(layer_image, ($img, i) => {
             let load_img = get(find($img, ".load-img"), 0);
             let overlay = get(find($img, ".layer-image-overlay"), 0);
             let clientRect = offset($img);
 
-            _images.push({
+            _images[i] = {
                 overlay, load_img,
                 target: $img,
                 clientRect,
                 isHero
-            });
+            };
 
             let _core_img = get(find($img, ".core-img"), 0);
             if (_is.def(_core_img)) {
@@ -69,43 +105,10 @@ let _load = () => {
         });
     });
 
-    let ready, resize;
-    on(window, 'resize', resize = () => {
-        toggleClass(_scrolldown, "action-btn-expand", width(window) <= 650);
-    });
-
-    on(window, 'scroll', ready = () => {
-        let _scrollTop = scrollTop(window);
-        hasClass(_navbar, "banner-mode") && addClass(_navbar, "navbar-focus") || toggleClass(_navbar, "navbar-focus", _scrollTop >= 5);
-        hasClass(_navbar, "navbar-show") && removeClass(_navbar, "navbar-show");
-        resize();
-
-        toggleClass(_actioncenter, "layer-action-center-show", _scrollTop > _focusPt * 4);
-        toggleClass(_actioncenter, "layer-action-center-hide", _scrollTop <= _focusPt * 4);
-
-        if (width(window) > 500) {
-            _images.forEach(data => {
-                if (hasClass(data.target, "effect-parallax")) {
-                    let { clientRect, load_img, overlay, isHero } = data;
-                    let { top, height } = clientRect;
-                    let dist = _scrollTop - top + 60;
-
-                    if (dist >= -_focusPt && dist <= height - _focusPt) {
-                        let value = Math.round(_map(_constrain(dist, 0, height), 0, height, 0, 1) * 100) / 100;
-
-                        isHero && style(overlay, { opacity: _map(value, 0, 0.75, 0.55, 0.15) });
-                        style(load_img, {
-                            transform: `translateY(${_map(_constrain(value - _map(60, 0, height, 0, 1), 0, 1), 0, 1, 0, height / 2)}px)`,
-                        });
-                    }
-                }
-            });
-        }
-    });
-
     ready();
 };
-
+on(window, 'resize', resize);
+on(window, 'scroll', ready);
 _load();
 
 // tell Barba to use the prefetch module
@@ -121,11 +124,11 @@ barba.init({
         scrollTo(0, "800ms");
         done();
       },
-      enter({ current, next, trigger }) {
+      afterEnter({ current, next, trigger }) {
         // do something with `next.container` for your enter transition
         // then return a promise or use `this.async()`
         const done = this.async();
-        _log("Barba JS is runnning");
+        _log(`Barba JS before: ${_images.length}`);
         _load();
         done();
 
