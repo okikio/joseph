3cee849433de33740a420b352833b3cd33944c5c
okikio <femikiki@gmail.com>
2020-03-14 02:41:56 -0400

    Updated browser list, remove general opts

--- browserlist.js
+++ browserlist.js
@@ -3,26 +3,23 @@ let browserlist = {
     "general": ["defaults, IE 8"]
 };
 
-let babelConfig = ["modern", "general"]
-.reduce((acc, type) => {
-    acc[type] = {
-        "babelrc": false,
-        "presets": [
-            ["@babel/preset-env", {
-                "useBuiltIns": false,
-                "modules": 'false',
-                "targets": {
-                    "browsers": browserlist[type]
-                }
-            }]
-        ]
-    };
-    return acc;
-}, {
-    "node": {
-        "presets": ["@babel/preset-env"]
-    }
-});
-
-let _export = { babelConfig, ...browserlist };
-module.exports = _export;
+module.exports = { 
+    "babelConfig": {
+        "modern": {
+            "babelrc": false,
+            "presets": [
+                ["@babel/preset-env", {
+                    "useBuiltIns": false,
+                    "modules": 'false',
+                    "targets": {
+                        "browsers": browserlist.modern
+                    }
+                }]
+            ]
+        },
+        "node": {
+            "presets": ["@babel/preset-env"]
+        }
+    }, 
+    ...browserlist 
+};
