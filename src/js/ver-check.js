9105886c762649a1b7bf672c2424d431125ddc9f
Okiki Ojo <femikiki@gmail.com>
2020-03-23 15:45:12 -0400

    Faster check for WebP support

--- src/js/ver-check.js
+++ src/js/ver-check.js
@@ -34,26 +34,13 @@ export let _require = (src, fn) => {
 };
 
 // Test for webp support
-(() => {
-    // If the browser doesn't has the method createImageBitmap, you can't display webp format
-    if (!window.createImageBitmap) {
-        window.WebpSupport = false;
-        return;
-    }
-
-    // Base64 representation of a white point image
-    let webpdata = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoCAAEAAQAcJaQAA3AA/v3AgAA=';
-
-    // Retrieve the Image in Blob Format
-    fetch(webpdata)
-        .then(response => response.blob())
-        .then(blob => {
-            // If the createImageBitmap method succeeds, return true, otherwise false
-            createImageBitmap(blob)
-                .then(() => { window.WebpSupport = true; })
-                .catch(() => { window.WebpSupport = false;  });
-        });
-}) ();
+try {
+    window.WebpSupport = document.createElement('canvas')
+                                 .toDataURL('image/webp')
+                                 .indexOf('data:image/webp') == 0;
+} catch (e) {
+    window.WebpSupport = false;
+}
 
 // Test for IE and older versions of Edge
 if (/msie|trident|edge/g.test(userAgent.toLowerCase()) || !window.isModern) {
