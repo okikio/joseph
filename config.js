651161f3cb1bd097c12eb34973b50a6d3de441f8
Okiki <femikiki@gmail.com>
2020-04-11 21:52:25 +0000

    Now should work now

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
-let env = process.env;
-if (!('dev' in env)) { 
-    console.log("dotenv ran");
-    require('dotenv').config(); 
-}
+let { env } = process;
+if (!('dev' in env)) require('dotenv').config();
 let dev = 'dev' in env && env.dev.toString() === "true";
 let debug = 'debug' in env && env.debug.toString() === "true";
 let githubPages = 'github_pages' in env && env.github_pages.toString() === "true";
 
 let env_vars = { dev, debug, githubPages };
-console.log("Enviroment Variables: ", env_vars, process.env['github_pages']);
+console.log("Enviroment Variables: ", env_vars);
 let _exports = {
     ...env_vars,
     "websiteURL": "https://josephojo.com",
