65d97b50d9e0759eb21b97eb5102dc276a5a96fc
Okiki <okikio.dev@gmail.com>
2021-06-27 01:19:37 +0000

    update sarbot video

---
+++ repl.cjs
@@ -0,0 +1,21 @@
+// import cloudinary from "cloudinary";
+const cloudinary = require("cloudinary");
+
+cloudinary.v2.config({
+    cloud_name: "",
+    api_key: "",
+    api_secret: ""
+});
+cloudinary.v2.uploader.explicit("joseph-projects/sarbot/Joseph_s_Trimmed_Video",
+    {
+        resource_type: "video", type: "upload",
+        eager_async: true,
+        eager: [
+            { format: "mp4", quality: "auto:low", video_codec: "auto", raw_transformations: "f_mp4,vc_auto,q_auto:low" },
+        ]
+    },
+    function (error, result) { 
+        if (error) return console.warn(error);
+        console.log(result); 
+    }
+);
